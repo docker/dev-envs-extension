825a3aa5143b0d3ac3b821210559e64d332a5cf0
Laura Brehm <laurabrehm@hey.com>
2022-11-01 14:20:02 +0100

    Refactor icon style
    Signed-off-by: Laura Brehm <laurabrehm@hey.com>

--- src/common/consts.ts
+++ src/common/consts.ts
@@ -2,6 +2,7 @@ export const BUTTON_ID = 'dev-envs-button';
 export const BUTTON_TITLE = 'Open in Docker Dev Environments';
 export const BUTTON_TEXT = 'Open in Dev Environments';
 export const DEVENVS_BUTTON_STYLE = 'dev-envs-button';
+export const DEVENVS_ICON_STYLE = 'dev-envs-icon';
 
 export const BITBUCKET_BUTTON_STYLE = 'bb-button';
 export const GITHUB_BUTTON_STYLE = 'gh-button';
--- src/common/icon.ts
+++ src/common/icon.ts
@@ -1,7 +1,9 @@
+import { DEVENVS_ICON_STYLE } from './consts';
+
 export const renderIcon = (size: number = 20) => {
   const icon = document.createElement('img');
   icon.src = 'https://github.com/docker/awesome-compose/raw/master/icon_devenvs.svg';
-  icon.className = 'dev-env-icon';
+  icon.className = DEVENVS_ICON_STYLE;
   icon.width = size;
   icon.height = size;
 
