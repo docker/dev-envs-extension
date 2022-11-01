5f7e781981a8cd43d624f025da4ec673189091ee
Laura Brehm <laurabrehm@hey.com>
2022-11-01 13:48:38 +0100

    Remove PR page handlers for `Gitlab` and `Bitbucket`
    ... until backend support in DevEnvs is added
    
    Signed-off-by: Laura Brehm <laurabrehm@hey.com>

--- src/devEnvs.ts
+++ src/devEnvs.ts
@@ -1,8 +1,6 @@
-import { BitbucketPRHandler } from './handler/bitbucket/bitbucketPRHandler';
 import { BitbucketRepoHandler } from './handler/bitbucket/bitbucketRepoHandler';
 import { GithubPRHandler } from './handler/github/githubPRHandler';
 import { GithubRepoHandler } from './handler/github/githubRepoHandler';
-import { GitlabPRHandler } from './handler/gitlab/gitlabPRHandler';
 import { GitlabRepoHandler } from './handler/gitlab/gitlabRepoHandler';
 import { Handler } from './handler/handler';
 
@@ -10,9 +8,7 @@ const handlers: Handler[] = [
   new GithubPRHandler(),
   new GithubRepoHandler(),
   new BitbucketRepoHandler(),
-  new BitbucketPRHandler(),
   new GitlabRepoHandler(),
-  new GitlabPRHandler(),
 ];
 
 const main = () => {
